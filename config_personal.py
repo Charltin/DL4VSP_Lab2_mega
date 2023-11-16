2b47e7a52524440be0c6f488bd56339b9ae44392
charltin <2268588240@qq.com>
2023-11-16 19:41:45 +0100

    add: some configs about permission

---
+++ config_personal.py
@@ -0,0 +1,11 @@
+# CONFIGS
+# remenber not upload it
+
+# TOKEN remember to use this token instead of using password
+token = "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"
+
+# git add .
+# git commit -m "[commit message]"
+# git push 
+# user name: charltin
+# user password: [token]
