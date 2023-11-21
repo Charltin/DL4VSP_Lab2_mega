94c68af267283c00e0588ec259081aee02c7e6fa
charltin <2268588240@qq.com>
2023-11-21 16:41:15 +0100

    commit: changes

--- config_personal.py
+++ config_personal.py
@@ -2,7 +2,7 @@
 # remenber not upload it
 
 # TOKEN remember to use this token instead of using password
-token = "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"
+token = "in email"
 
 # git add .
 # git commit -m "[commit message]"
